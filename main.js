2ce76fbe9ade83cd8f78126bd9fd60467a12935b
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2022-12-26 16:44:07 -0500

    created function to parse current day weather data

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-import './style.css'
-import getWeatherInfo from './getWeatherInfo'
+import getWeatherInfo from './weatherInfo'
 
+navigator.geolocation.getCurrentPosition(positionSuccess, () => {});
 
-
+function positionSuccess({ coords }) {
+    getWeatherInfo(
+    coords.latitude,
+    coords.longitude,
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  )
+    .then(data => console.log(data))
+    .catch((e) => {
+      console.error(e);
+      alert("Error: " + e.message);
+    });
+}
--- weatherInfo.js
+++ weatherInfo.js
@@ -1,15 +1,48 @@
 import axios from "axios";
 
-const API = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,visibility,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&current_weather=true&timeformat=unixtime"
+const API =
+  "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,visibility,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&current_weather=true&timeformat=unixtime";
 
-export default getWeatherInfo = (lat, lon, timezone) => {
-  return axios.get(API, {
-    params: {
+export default async function getWeatherInfo(lat, lon, timezone) {
+  return axios
+    .get(API, {
+      params: {
         latitude: lat,
         longitude: lon,
-        timezone: timezone
-    }
-  }).then(({data}) => {
-    return data;
-  });
-};
+        timezone: timezone,
+      },
+    })
+    .then(({ data }) => {
+      return data;
+      return {
+        current: getCurrentData(data),
+      };
+    });
+}
+
+function getCurrentData({ current_weather, daily }) {
+  const { temperature: currentTemp, windspeed, weathercode } = current_weather;
+  const {
+    apparent_temperature_max: FLHigh,
+    apparent_temperature_min: FLLow,
+    precipitation_sum: precip,
+    sunrise,
+    sunset,
+    temperature_2m_max: maxTemp,
+    temperature_2m_min: minTemp,
+  } = daily;
+
+  return {
+    currentTemp: currentTemp,
+    maxTemp: maxTemp[0],
+    minTemp: minTemp[0],
+    FLHigh: FLHigh[0],
+    FLLow: FLLow[0],
+    windSpeed: windspeed,
+    sunrise: sunrise[0],
+    sunset: sunset[0],
+    precip: precip[0],
+    weatherIcon: weathercode
+  }
+
+}
